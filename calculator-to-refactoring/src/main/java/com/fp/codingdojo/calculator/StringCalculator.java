17c175d7509c0fbc16be456f709c904f1d15c16b
Marcin Butora <36995841+marcinbutora@users.noreply.github.com>
2022-12-28 16:05:24 +0100

    solution

--- calculator-to-refactoring/src/main/java/com/fp/codingdojo/calculator/StringCalculator.java
+++ calculator-to-refactoring/src/main/java/com/fp/codingdojo/calculator/StringCalculator.java
@@ -4,37 +4,41 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
-	public static void main(String[] args) {
-		sum(new String[]{""});
-		sum(new String[]{"1"});
-		sum(new String[]{"1,2"});
-		sum(new String[]{"4"});
-		sum(new String[]{"5,7"});
-		sum(new String[]{"5,17"});
-		sum(new String[]{"5,6,7"});
-		sum(new String[]{"5,6,7"});
-		sum(new String[]{"5,6,7,8,9,10"});
-		sum(new String[]{"5,6\n7,8\n9,10"});
-	}
 
-	// TODO: 28/12/2022 Change string[] to string (one string as a parameter)
-	public static String sum(String[] args) {
-		System.out.println("input: " + args[0]);
+    public static void main(String[] args) {
+        sum(new String[]{""});
+        sum(new String[]{"1"});
+        sum(new String[]{"1,2"});
+        sum(new String[]{"4"});
+        sum(new String[]{"5,7"});
+        sum(new String[]{"5,17"});
+        sum(new String[]{"5,6,7"});
+        sum(new String[]{"5,6,7"});
+        sum(new String[]{"5,6,7,8,9,10"});
+        sum(new String[]{"5,6\n7,8\n9,10"});
+    }
 
-		if (args[0].equals("")) {
-			System.out.println("output: 0");
-			return String.valueOf(0);
-		}
+    public static String sum(String[] args) {
+        String inputString = args[0];
+        System.out.println("input: " + inputString);
+        String result2 = inputString.equals("")
+                ? "0"
+                : sum(inputString);
 
-		String[] numbers = args[0].split(",");
+        System.out.println("output " + result2);
+        return result2;
+    }
 
-		Integer result = Arrays.stream(numbers)
-				.map(s -> s.split("\n"))
-				.flatMap(strings -> Arrays.stream(strings))
-				.mapToInt(value -> Integer.parseInt(value))
-				.sum();
+    private static String sum(String inputString) {
+        final String DELIMITERS = "\n";
+        String[] numbers = inputString.split(",");
 
-			System.out.println("output " + result);
-			return String.valueOf(result);
-	}
+        Integer resultSum = Arrays.stream(numbers)
+                .map(s -> s.split(DELIMITERS))
+                .flatMap(Arrays::stream)
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        return String.valueOf(resultSum);
+    }
 }
